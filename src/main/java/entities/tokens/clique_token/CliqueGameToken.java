69d04edc432d9cb0002079890cc09c9099d5698d
Ioan <ioan.sava28@gmail.com>
2020-04-03 19:42:02 +0300

    update

--- src/main/java/entities/tokens/clique_token/CliqueGameToken.java
+++ src/main/java/entities/tokens/clique_token/CliqueGameToken.java
@@ -1,19 +1,47 @@
-package entities.tokens;
+package entities.tokens.clique_token;
 
+import entities.tokens.ArithmeticProgressionToken;
+import entities.tokens.Token;
 import lombok.Getter;
+import lombok.Setter;
+import org.jgrapht.graph.DefaultEdge;
 
 /**
  * An instance of this class will hold
+ * two nodes which form
  * an edge of an undirected graph.
  *
  * @author Ioan Sava
  */
 @Getter
 public class CliqueGameToken extends Token {
-    
+    private Node firstNode;
+    private Node secondNode;
+
+    public CliqueGameToken(Node firstNode, Node secondNode) {
+        this.firstNode = firstNode;
+        this.secondNode = secondNode;
+    }
+
+    public CliqueGameToken(int firstNodeLabel, int secondNodeLabel) {
+        this.firstNode = new Node(firstNodeLabel);
+        this.secondNode = new Node(secondNodeLabel);
+    }
+
+    public int compareTo(CliqueGameToken token) {
+        if (this.firstNode.getLabel() == token.getFirstNode().getLabel()) {
+            return this.secondNode.getLabel() - token.getSecondNode().getLabel();
+        }
+        return this.firstNode.getLabel() - token.getFirstNode().getLabel();
+    }
 
     @Override
     public int compareTo(Token token) {
-        return 0;
+        return compareTo((CliqueGameToken) token);
+    }
+
+    @Override
+    public String toString() {
+        return "CGToken(" + firstNode + "," + secondNode + ')';
     }
 }
--- src/main/java/entities/tokens/clique_token/Node.java
+++ src/main/java/entities/tokens/clique_token/Node.java
@@ -1,4 +1,26 @@
 package entities.tokens.clique_token;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * A node of an undirected graph.
+ *
+ * @author Ioan Sava
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Node {
+    private int label;
+
+    @Override
+    public String toString() {
+        return "N(" + label + ")";
+    }
 }
